2fe701038d740be9960c25eb58f50be14f88c109
HenrickyL <HenrickyL1@gmail.com>
2024-06-15 07:33:36 -0300

    base service & prettier

--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -1,5 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  create(data) {
+    return this.userService.create(data);
+  }
 }
