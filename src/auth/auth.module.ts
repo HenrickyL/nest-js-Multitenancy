51e2df7467e712edffca06fb0ba1815ff61c0cba
HenrickyL <HenrickyL1@gmail.com>
2024-06-15 09:40:57 -0300

    add controller partner user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { PartnerUsersController } from './admin-users/partner-users.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UsersController],
+  controllers: [UsersController, PartnerUsersController],
   providers: [UsersService],
 })
 export class AuthModule {}
