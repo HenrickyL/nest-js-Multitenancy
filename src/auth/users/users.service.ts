4069841de140c26c542bcf97f61067a9f84f289b
HenrickyL <HenrickyL1@gmail.com>
2024-06-15 09:12:53 -0300

    add password crypt

--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserRoles } from './user-roles';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
@@ -10,8 +10,12 @@ export class UsersService {
     return this.prismaService.user.create({
       data: {
         ...data,
+        password: this.generateHash(data.password),
         roles: [UserRoles.USER],
       },
     });
+  }
+  private generateHash(password: string) {
+    return bcrypt.hashSync(password, 10);
   }
 }
